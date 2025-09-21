449384a1fb4f1e7912e2043923e73494c3a3b9fc
Aviva1234 <155398801+Aviva1234@users.noreply.github.com>
2025-09-21 11:26:11 -0400

    Add files via upload

--- starShooter.py
+++ starShooter.py
@@ -1,3 +1,4 @@
+import time
 import pgzrun
 import random
 #.wav & .ogg files can be used for sound effects and music
@@ -6,6 +7,7 @@ HEIGHT = 500
 game = True
 score = 0
 level = 1
+lives = 3
 wasps = []
 m = Actor("starshooter")
 m.x = 300
@@ -14,6 +16,9 @@ bullets = []
 def draw():
     screen.blit("space", (0,0))
     global score
+    global lives
+    global game
+    global level
     screen.draw.text(f"Score: {score}", (10,10), color="white")
     if game:
         m.draw()
@@ -21,10 +26,22 @@ def draw():
             b.draw()
         for w in wasps:
             w.draw()
+    screen.draw.text(f"Lives: {lives}", (500,10), color="red")
+    screen.draw.text(f"Level: {level}", (250,10), color="yellow")
+    if lives < 0:
+        screen.draw.text(f"Game Over! Your score was {score}",(100,100),color="purple", fontsize=40)
+        time.sleep(2)
+        game = False
+    if level > 4:
+        screen.draw.text(f"You Win! Your score was {score}",(100,100),color="blue", fontsize=40)
+        time.sleep(2)
+        game = False
 def update():
     global game
     global score
     global level
+    global lives
+    global bullets
     if keyboard.right:
         m.x += 5
     elif keyboard.left:
@@ -41,8 +58,8 @@ def update():
         w.y+=0.2
         if w.y > 550:
             game = False
-    for b in bullets:
-        for w in wasps:
+    for w in wasps:
+        for b in bullets:
             if b.colliderect(w):
                 bullets.remove(b)
                 wasps.remove(w)
@@ -50,6 +67,10 @@ def update():
             if len(wasps) == 0:
                 level+=1
                 spawn_wasps()
+    for w in wasps:
+        if w.colliderect(m):
+            wasps.remove(w)
+            lives -= 1
 def on_key_down(key):
     if key == keys.SPACE:
         b = Actor("bullet")
@@ -59,11 +80,12 @@ def on_key_down(key):
         sounds.eep.play()
 def spawn_wasps():
     global level
-    for o in range(level):
-        for i in range(0,6):
-            w = Actor("wasp")
-            w.x = 50+i*100
-            w.y = 50+o*70
-            wasps.append(w)
+    if level <= 4:
+        for o in range(level):
+            for i in range(0,6):
+                w = Actor("wasp")
+                w.x = 50+i*100
+                w.y = 50+o*70
+                wasps.append(w)
 spawn_wasps()
 pgzrun.go()
